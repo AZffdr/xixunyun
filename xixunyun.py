2807eaf08f3a15f13e453521b749bfe02bc02446
AZffdr <152608992+AZffdr@users.noreply.github.com>
2023-12-06 16:04:22 +0800

    xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -22,11 +22,11 @@ headers = {'Content-Type': 'application/json'}
 
 response = requests.post(url='https://api.xixunyun.com/login/api?from=app&version=5.1.1&platform=android', headers=headers, data=json.dumps(data))
 if response.status_code==200:
-  re = requests.get(url='https://api.xixunyun.com/signin_rsa?from=app&version=5.1.1&platform=android&entrance_year=0&graduate_year=0&school_id='+school_id, headers=headers, cookies=response.cookies)
+  re = requests.get(url='https://api.xixunyun.com/signin_rsa?token="b1b0be25325e42429f468fb0790318cd"&from=app&version=5.1.1&platform=android&entrance_year=0&graduate_year=0&school_id='+school_id, headers=headers, cookies=response.cookies)
 print(response.json()["data"])
 
 SCKEY=os.environ["SCKEY"]
 if len(SCKEY) >= 1:
   url = 'https://sctapi.ftqq.com/'+SCKEY+'.send'
-  requests.post(url, data={"title": "习讯云签到提醒"+print(data.account), "desp": response.json()["data"]})
+  requests.post(url, data={"title": "习讯云签到提醒", "desp": response.json()["data"]},"message"=re.message)
 
