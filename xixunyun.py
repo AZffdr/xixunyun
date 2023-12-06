102b78fb9a3d9afafb861b474bf8bd7b90d3db6d
AZffdr <152608992+AZffdr@users.noreply.github.com>
2023-12-06 16:16:44 +0800

    xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -18,9 +18,9 @@ data = {
   "latitude":latitude,
   "address_name":os.environ["ADDRESS_NAME"] 
 }
-headers = {'Content-Type': 'application/json'}
+headers = {'Content-Type': 'application/json','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0'}
 
-response = requests.post(url='https://api.xixunyun.com/login/api?from=app&version=5.1.1&platform=android', headers=headers, data=json.dumps(data))
+response = requests.post(url='https://api.xixunyun.com/login/api?from=app&version=5.1.1&platform=android', headers=headers, data=data)
 if response.status_code==200:
   re = requests.get(url='https://api.xixunyun.com/signin_rsa?token="b1b0be25325e42429f468fb0790318cd"&from=app&version=5.1.1&platform=android&entrance_year=0&graduate_year=0&school_id='+school_id, headers=headers, cookies=response.cookies)
 print(response.json()["data"])
@@ -28,5 +28,4 @@ print(response.json()["data"])
 SCKEY=os.environ["SCKEY"]
 if len(SCKEY) >= 1:
   url = 'https://sctapi.ftqq.com/'+SCKEY+'.send'
-  requests.post(url, data={"title": "习讯云签到提醒", "desp":re.message)
-
+  requests.post(url, data={"title": "习讯云签到提醒", "desp":re.message})
